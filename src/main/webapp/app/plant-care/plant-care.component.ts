9ce43f7504ed4493d3dab2f66eb5d64676b6f132
Astrid Lindh <slagverkare.astrid.lindh@gmail.com>
2021-04-23 00:07:13 +0200

    Added plantcare but page not working yet

---
+++ src/main/webapp/app/plant-care/plant-care.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // det här lade jag till
+
+@Component({
+  selector: 'jhi-plant-care',
+  templateUrl: './plant-care.component.html',
+  styleUrls: ['./plant-care.component.scss'],
+})
+
+//export class PlantCareComponent{}
+export class PlantCareComponent {
+  constructor() {
+    return new PlantCareComponent();
+  }
+}
+
+/*export class PlantCareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}*/

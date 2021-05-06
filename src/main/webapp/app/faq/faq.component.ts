34c4dde238173c9b34c221f4fc3e6778070b349b
Nour95-omar <nbashar@kth.se>
2021-05-06 19:17:57 +0200

    about us and FAQ is done

--- src/main/webapp/app/faq/faq.component.ts
+++ src/main/webapp/app/faq/faq.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class FAQComponent {
   constructor(private router: Router) {}
-
+  //
   // myFunc()  {
   //   var ans = ((document.getElementById("answer") as HTMLInputElement).value);
   //   console.log(ans);
